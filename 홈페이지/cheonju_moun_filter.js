4c714cd5d6bd9c2306de98927861b9680b8c4b9f
eorhks45@naver.com <eorhks45@naver.com>
2022-05-26 21:40:51 +0900

    카테고리 필터가 작동을 안하네요..

--- 홈페이지/cheonju_moun_filter.js
+++ 홈페이지/cheonju_moun_filter.js
@@ -1,24 +1,5 @@
 
-var mountain = [
-    {id : 0, local : 서원구, title : '팔봉산', height : 297.4 },
-    {id : 1, local : 서원구, title : '망월산', height : 367.1 },
-    {id : 2, local : 서원구, title : '용덕산', height : 440.7 },
-    {id : 3, local : 서원구, title : '봉무산' },
-    {id : 4, local : 서원구, title : '부모산' },
-    {id : 5, local : 서원구, title : '명심산' },
-    {id : 6, local : 서원구, title : '은적산' },
-    {id : 7, local : 서원구, title : '두루봉산' },
-    {id : 8, local : 서원구, title : '병마산' },
-    {id : 9, local : 서원구, title : '동림산' },
-    {id : 10, local : 서원구, title : '망덕산' },
-    {id : 11, local : 서원구, title : '마봉산' },
-    {id : 12, local : 서원구, title : '돛대산' },
-    {id : 13, local : 서원구, title : '아미산' },
-    {id : 14, local : 서원구, title : '망덕산' },
-    {id : 15, local : 서원구, title : '계미산' },
-    {id : 16, local : 서원구, title : '학천산' }
-  ]
-
+filterObjects("all");
 
   function filterObjects(c){
     var x, i;
@@ -31,7 +12,25 @@ var mountain = [
   }
 
   function addClass(element, name){
-    var i, ar1, arr2;
+    var i, arr1, arr2;
     arr1 = element.className.Split(" ");
+    arr2 = name.spilt(" ");
+    for (i = 0; i < arr2.lentth; i++){
+      if(arr1. indexOf(arr2[i]) == -1){
+        element.className += " " + arr2[i];
+      } 
+    }
+  }
+
+  function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.spilt(" ");
+    arr2 = name.spilt(" ");
+    for (i =0; i < arr2.length; i++){
+      while (arr1.indexOf(arr2[i]) > -1){
+        arr1.splice(arr1.indexof(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join("");
   }
 
